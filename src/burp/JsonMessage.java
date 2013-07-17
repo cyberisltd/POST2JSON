e25b59e2c4e7dfae541d14dc0fb1df7824d039a8
cyberisltd <github@cyberis.co.uk>
2013-07-17 21:38:54 +0100

    Version 1.0

---
+++ src/burp/JsonMessage.java
@@ -0,0 +1,97 @@
+/*
+ Copyright (C) 2013  Cyberis Ltd. Author geoff.jones@cyberis.co.uk
+
+ This file is part of POST2JSON, a Burp Suite extender to convert a POST 
+ request to a JSON message.
+
+ POST2JSON is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ POST2JSON is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package burp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author geoff.jones@cyberis.co.uk
+ * @Copyright Cyberis Limited 2013
+ *
+ * Class to represent a JSON message. Takes a HTTP POST Request and converts to
+ * a JSON message. Moves any .NET __RequestVerificationToken to the request
+ * headers if found
+ */
+class JsonMessage {
+
+    private final List<IParameter> myParameters;
+    private final List<IParameter> myJsonParameters;
+    private final BurpExtender myExtender;
+    private final List<String> myHeaders;
+    private final IRequestInfo myRequest;
+    private final byte[] body;
+
+    public JsonMessage(BurpExtender extender, IHttpRequestResponse message) {
+        myExtender = extender;
+        myRequest = myExtender.getHelpers().analyzeRequest(message.getRequest());
+        myHeaders = new ArrayList<>();
+        //myParamerts will hold all parameters EXCEPT JSON (body) parameters
+        myParameters = new ArrayList<>();
+        myJsonParameters = new ArrayList<>();
+
+        //Change the content type, retain all other headers
+        List<String> headers = myRequest.getHeaders();
+        for (String i : headers) {
+            if (i.startsWith("Content-Type:")) {
+                myHeaders.add("Content-Type: application/json");
+            } else {
+                myHeaders.add(i);
+            }
+        }
+
+        //Construct parameters, adding the verification token to the headers if found
+        List<IParameter> parameters = myRequest.getParameters();
+        for (IParameter i : parameters) {
+            if (i.getType() == IParameter.PARAM_BODY) {
+                if (i.getName().equals("__RequestVerificationToken")) {
+                    myHeaders.add("__RequestVerificationToken: " + i.getValue());
+                } else {
+                    myJsonParameters.add(i);
+                }
+            } else {
+                myParameters.add(i);
+            }
+        }
+
+        //Build the JSON body from the parameters
+        StringBuilder sb = new StringBuilder();
+        sb.append("{");
+        String delim = "";
+        for (IParameter i : myJsonParameters) {
+            sb.append(delim).append("\"").append(i.getName()).append("\":");
+            sb.append("\"").append(i.getValue()).append("\"");
+            delim = ",";
+        }
+        sb.append("}");
+
+        //Convert the constructed body to a byte[]
+        body = sb.toString().getBytes();
+    }
+
+    /**
+     * Get the HTTP generated request
+     *
+     * @return byte[]
+     */
+    public byte[] getMessage() {
+        return myExtender.getHelpers().buildHttpMessage(myHeaders, body);
+    }
+}
